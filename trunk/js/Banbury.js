325e5c569103fc9535590f06025d70715ff54681
e-man <e-man@0ad31202-636c-4a70-85b4-5c1ed2c399fd>
2007-08-12 18:29:24 +0000

    OOP in Banbury.js entfernt

--- trunk/js/Banbury.js
+++ trunk/js/Banbury.js
@@ -31,308 +31,248 @@ function winkeldiff(winkel1, winkel2) {
 	return r;
 }
 
-function bild(bname, x, y) {
-	this.x = x;
-	this.y = y;
-	this.winkel = 0.0;
-	this.zielwinkel = 0.0;
-	this.objektid = bname;
-	this.richtung = 0;
+// derzeitiger Winkel der Komponenten
+var winkel = new Array();
 
-	this.positionXY = function(posx, posy) {
-		bild = document.getElementById(this.objektid);
-		bild.style.left = posx - bild.width/2;
-		bild.style.top = posy - bild.height/2;
-	}
+// einzunehmender Winkel jeder Komponente
+var zielwinkel = new Array();
 
-	this.bewege = function() {
-		while (this.winkel < -180) this.winkel += 360;
-		while (this.winkel > 180) this.winkel -= 360;
+// Feld aller Objektnamen
+var objektid = new Array();
+
+// Bewegungsrichtungen der Komponenten
+var richtung = new Array();
+
+// Zentrum der Grafiken
+var center = 150;
+
+// Radius
+var radius = 125.0;
+
+// In diesem Winkel hält sich nur das zur Zeit ausgewählte Symbol auf
+var sperrwinkel = 120;
+
+// die Anzahl der Komponenten
+var anzahlTeile = 0;
+
+// die Nummer der gerade aktiven Komponente
+var aktivesTeil = 0;
+
+// Liste der auszuführenden Bewegungsbefehle
+var bewegungen = new Array();
+
+// Sucht die Position des Bildes mit dem Namen bname
+
+function indexByObjectid(bname) {
+	r = -1;
+	for (var i=0; i<objektid.length; i++) if (objektid[i] == bname) r = i;
+	return r;
+}
+
+
+// Positioniere das Bild zentriert auf den Koordinaten x und y
+
+function bildpositionXY(bname, x, y) {
+	bild = document.getElementById(bname);
+	bild.style.left = x - bild.width/2;
+	bild.style.top = y - bild.height/2;
+}
+
+
+// Bewege ein Bild
+
+function bildBewegung(bindex) {
+	if (bindex > -1) {
+		while (winkel[bindex] < -180) winkel[bindex] += 360;
+		while (winkel[bindex] > 180) winkel[bindex] -= 360;
 		r = false;
-		if (this.richtung == 0) {
-			if (Math.abs(this.zielwinkel - this.winkel) > 180) this.richtung = -1; else this.richtung = 1;
+		if (richtung[bindex] == 0) {
+			if (Math.abs(zielwinkel[bindex] - winkel[bindex]) > 180) richtung[bindex] = -1; else richtung[bindex] = 1;
 		}
-		if (winkeldiff(this.zielwinkel, this.winkel) >= 2 ) {
-			this.winkel += this.richtung*Math.floor(1+Math.sqrt(winkeldiff(this.zielwinkel, this.winkel)));
+		if (winkeldiff(zielwinkel[bindex], winkel[bindex]) >= 2 ) {
+			winkel[bindex] += richtung[bindex]*Math.floor(1+Math.sqrt(winkeldiff(zielwinkel[bindex], winkel[bindex])));
+			bildpositionXY(objektid[bindex],
+				Math.floor(center + radius * Math.cos((-90+winkel[bindex])*3.14/180.0)),
+				Math.floor(center + radius * Math.sin((-90+winkel[bindex])*3.14/180.0)));
 			r = true;
 		}
-		this.positionXY(
-			Math.floor(center + radius * Math.cos((-90+this.winkel)*3.14/180.0)),
-			Math.floor(center + radius * Math.sin((-90+this.winkel)*3.14/180.0)));
 		return r;
 	}
 }
 
-function hardwaresystemUI() {
 
-	anzahlTeile = 0;
-	aktivesTeil = 0;
-	aktiv = "";
 
-	bewegungen = new Array();
-	bilder = new Array();
+// Erzeuge das Objekt zur Repräsentation des Auswahlrahmens
 
-	this.add = function() {
-		hwuielem = document.getElementById("hwui");
-		neuessymbol = this.teilbild("fragezeichen.png", "neu");
-		hwuielem.appendChild(neuessymbol);
-	}
+function auswahlrahmen() {
+	arahmen = document.createElement("img");
+	arahmen.src = "auswahlrahmen.png";
+	arahmen.style.left=88;
+	arahmen.style.top=-20;
+	arahmen.style.zIndex = 99;
+	arahmen.style.position="relative";
 
-	this.del = function() {
-		if (bilder[aktivesTeil].objektid.substr(0,2) != "QQ") {
-			bilderneu = new Array();
-			for (var i = 0; i < anzahlTeile; i++) {
-				if (i != aktivesTeil) {
-					bilder[i].richtung = 0;
-					bilderneu.push(bilder[i]);
-				} else {
-					b = document.getElementById("ank" + bilder[i].objektid);
-					pank = b.parentNode;
-					pank.removeChild(b);
-				}
-			}
-			anzahlTeile = bilderneu.length;
-			bilder = bilderneu;
-			aktivesTeil = aktivesTeil % anzahlTeile
-			bewegungen = new Array();
-			bewegungen.push("nix,distribute");
-		}
-	}
+	return arahmen;
+}
 
-	this.nameAktivesTeil = function() {
-		name = bilder[aktivesTeil].objektid;
-		return name;
-	}
 
-	this.pl = function() {
-		this.springeZu((aktivesTeil + 1) % anzahlTeile);
-	}
+// Erstellt eine neue Komponente
 
-	this.mn = function() {
-		this.springeZu((aktivesTeil + anzahlTeile - 1) % anzahlTeile);
-	}
-
-	this.springeZuNamen = function(teilname) {
-		ziel = -1;
-		for (var i = 0; i < anzahlTeile; i++) {
-			if (bilder[i].objektid == teilname) { ziel = i; }
-		}
-		if (ziel >= 0) this.springeZu(ziel);
-	}
-
-	//Der Benutzer klickt im großen Kreis auf ein Symbol
-	//es drehen sich alle Symbole im äußeren Kreis,
-	//bis das angeklickte Symbol im Bearbeiten-Feld steht
-	this.springeZu = function (teilnummer) {
-		mitte = anzahlTeile / 2;
-
-		//abstand zw. aktivem und teiln muss kl sein als mitte
-		abstand = (aktivesTeil - teilnummer + anzahlTeile) % anzahlTeile;
-		if (abstand <= mitte) r = 1; else r = -1;
-
-		aktivesTeil = teilnummer % anzahlTeile;
-		for (var i = 0; i < anzahlTeile; i++) {
-			bilder[i].richtung = r;
-		}
-		bewegungen = new Array();
-		bewegungen.push("nix,distribute");
-	}
-
-	this.bewegen = function() {
-		neue_bewegungen = new Array();
-		for (var i = 0; i < bewegungen.length; i++) {
-			zeile = bewegungen[i];
-			objekt = zeile.substr(0, zeile.indexOf(","));
-			ani = zeile.substr(zeile.indexOf(",")+1, zeile.length);
-			if (ani == "distribute") {
-				bilder[aktivesTeil].zielwinkel = 0;
-				if (anzahlTeile > 2) {
-					winkelschritt = 360 / anzahlTeile;
-					zielwinkel = winkelschritt;
-					if (zielwinkel < sperrwinkel / 2) {
-						zielwinkel = sperrwinkel / 2;
-						winkelschritt = (360-sperrwinkel) / (anzahlTeile - 2);
-					}
-					debugTxT = "aktivesTeil: " + aktivesTeil;
-					debugTxT = debugTxT + ",anzahlTeile: " + anzahlTeile;
-					for (var i = 1; i < anzahlTeile; i++) {
-						bilder[(aktivesTeil + i) % anzahlTeile].zielwinkel = zielwinkel;
-						//alert(zielwinkel + "," + winkelschritt);
-						debugTxT = debugTxT + ", bild[" + ((aktivesTeil + i) % anzahlTeile ) + "]="+ zielwinkel;
-						zielwinkel = (zielwinkel + winkelschritt) % 360;
-					}
-					debugTxT = debugTxT + ", " + zielwinkel;
-					dp = document.getElementById("debug");
-					dp.firstChild.nodeValue= "Debug: " + debugTxT;
-				} else {
-					if (anzahlTeile == 2) {
-						bilder[(aktivesTeil+1)%2].zielwinkel = 180;
-					}
-				}
-				for (var i = 0; i < anzahlTeile; i++) {
-					neue_bewegungen.push(bilder[i].objektid + ",move");
-				}
-			}
-			if (ani == "move") {
-				for (var i = 0; i < anzahlTeile; i++) {
-					if (bilder[i].bewege()) neue_bewegungen.push(zeile);
-				}
-			}
-		}
-		bewegungen = neue_bewegungen;
-	}
-
-	function loeschenFrage(teilenummer) {
-
-	}
-
-	function loeschen(teilenummer) {
-		hardwaresystemC.loesche(teilnummer);
-	}
-
-	function loeschanimation(teilenummer) {
-		//verkleinere das Symbol
-		//bewege alle Symbole auf neue Positionen
-	}
-
-	function neuanimation() {
-	}
-
-	this.init = function () {
-		hwuielem = document.getElementById("hwui");
-		hwuielem.style.backgroundImage = "url(kreishintergrund.png)";
-		hwuielem.style.height = "300px";
-		hwuielem.style.width = "300px";
-
-		hwuielem.appendChild(this.auswahlrahmen());
-		hwuielem.appendChild(this.teilbild("fragezeichen.png", "QQsystem"));
-		hwuielem.appendChild(this.teilbild("teil1.png", "festplatte"));
-		hwuielem.appendChild(this.teilbild("teil2.png", "cdrom"));
-		hwuielem.appendChild(this.teilbild("teil3.png", "mouse"));
-		hwuielem.appendChild(this.teilbild("teil4.png", "monitor"));
-		hwuielem.appendChild(this.teilbild("teil5.png", "festplatte2"));
-		hwuielem.appendChild(this.teilbild("teil6.png", "ram"));
-
-		bewegungen.push("nix,distribute");
-		aktiv = window.setInterval("hwsysc.animation()", 100);
-	}
-
-	this.stop = function() {
-		window.clearInterval(aktiv);
-	}
-
-	this.teilbild2 = function(src, name) {
-		tbimg = document.createElement("img");
-		tbimg.src = src;
-		tbimg.style.position="absolute";
-		tbimg.style.left = 210;
-		tbimg.style.top = 100;
-		tbimg.zIndex = 25;
-		tbimg.id = name;
-		/*abild = new bild(name, 150, 150);
-		abild.winkel = 0;
-		abild.zielwinkel = 0;
-		bilder.push(abild);*/
-
-		ank = document.createElement("a");
-		ank.setAttribute('href', 'javascript:hwsysc.jt("' + name + '");');
-		ank.appendChild(tbimg);
-		ank.setAttribute('id', 'ank' + name);
-
-		anzahlTeile++;
-
-		return ank;
-	}
+function teilbild(src, name) {
+	tbimg = document.createElement("img");
+	tbimg.src = src;
+	tbimg.style.position="absolute";
+	tbimg.style.left = 210;
+	tbimg.style.top = 100;
+	tbimg.style.borderWidth = 0;
+	tbimg.zIndex = 25;
+	tbimg.id = name;
+	/*winkel.push(-5);
+	zielwinkel.push(0);
+	objektid.push(name);
+	richtung.push(0);
+	*/
+	winkel.splice(aktivesTeil, 0, -5);
+	zielwinkel.splice(aktivesTeil, 0, 0);
+	objektid.splice(aktivesTeil, 0, name);
+	richtung.splice(aktivesTeil, 0, 0);
 	
-	this.teilbild = function(src, name) {
-		tbimg = document.createElement("img");
-		tbimg.src = src;
-		tbimg.style.position="absolute";
-		tbimg.style.left = 210;
-		tbimg.style.top = 100;
-		tbimg.zIndex = 25;
-		tbimg.id = name;
-		abild = new bild(name, 150, 150);
-		abild.winkel = 0;
-		abild.zielwinkel = 0;
-		bilder.push(abild);
+	ank = document.createElement("a");
+	ank.setAttribute('href', 'javascript:springeZuNamen("' + name + '");');
+	ank.setAttribute('id', 'ank' + name);
+	ank.appendChild(tbimg);
 
-		ank = document.createElement("a");
-		ank.setAttribute('href', 'javascript:hwsysc.jt("' + name + '");');
-		ank.appendChild(tbimg);
-		ank.setAttribute('id', 'ank' + name);
 
-		anzahlTeile++;
+	anzahlTeile = objektid.length;
 
-		return ank;
+	return ank;
+}
+
+
+function einzigartigerObjektname(vorlage) {
+	l = vorlage.length;
+	max = 0;
+	for (var i = 0; i < objektid.length; i++) {
+		objvorlage = objektid[i].substr(0, l);
+		if (objvorlage == vorlage) {
+			altmax = objektid[i].substr(l, objektid[i].length-1);
+			max = Math.max(max, altmax);
+		}
 	}
+	return vorlage + (max+1);
+}
 
-	this.auswahlrahmen = function() {
-		arahmen = document.createElement("img");
-		arahmen.src = "auswahlrahmen.png";
-		arahmen.style.left=88;
-		arahmen.style.top=-20;
-		arahmen.style.zIndex = 99;
-		arahmen.style.position="relative";
 
-		return arahmen;
+// Fügt ein leeres Symbol ein
+
+function add() {
+	hwuielem = document.getElementById("hwui");
+	name = einzigartigerObjektname("neu");
+	hwuielem.appendChild(teilbild("fragezeichen.png", name));
+	springeZuNamen(name);
+}
+
+
+function del() {
+	if (objektid[aktivesTeil].substr(0,2) != "QQ") {
+		b = document.getElementById("ank" + objektid[aktivesTeil]);
+		pank = b.parentNode;
+		pank.removeChild(b);
+		
+		winkel.splice(aktivesTeil, 1);
+		zielwinkel.splice(aktivesTeil, 1);
+		objektid.splice(aktivesTeil, 1);
+		richtung.splice(aktivesTeil, 1);
+		
+		anzahlTeile = objektid.length;
+		aktivesTeil = aktivesTeil % anzahlTeile;
+		bewegungen.push("distribute");
 	}
- }
+}
 
- function hardwaresystemC() {
+function drehpl() {
+	springeZu((aktivesTeil + 1) % anzahlTeile);
+}
 
-	 hwui = new hardwaresystemUI();
+function drehmn() {
+	springeZu((aktivesTeil + anzahlTeile - 1) % anzahlTeile);
+}
 
-	 function neu() {
-		 hardwaresystemUI.neuanimation();
-	 }
 
-	 function loesche(teilnummer) {
-		 //lösche das n-te Element (n = teilnummer) aus teile
-		 aktualisiereUI();
-	 }
+function springeZuNamen(teilname) {
+	ziel = indexByObjectid(teilname);
+	if (ziel >= 0) this.springeZu(ziel);
+}
 
-	 function aktualisiereUI() {
-		 hardwaresystemUI.anzahlTeile = teile.anzahl;
-		 loeschanimation(teilenummer);
-	 }
 
-	 this.init = function () {
-		hwui.init();
-	 }
+function springeZu(teilnummer) {
+	mitte = anzahlTeile / 2;
+	abstand = (aktivesTeil - teilnummer + anzahlTeile) % anzahlTeile;
+	if (abstand <= mitte) r = 1; else r = -1;
 
-	 this.animation = function () {
-		 hwui.bewegen();
-	 }
+	aktivesTeil = teilnummer % anzahlTeile;
+	for (var i = 0; i < anzahlTeile; i++) {
+		richtung[i] = r;
+	}
+	bewegungen.push("distribute");
+}
 
-	 this.stop = function() {
-		 hwui.stop();
-	 }
+function bewegen() {
+	aktion = bewegungen.shift();
+	if (aktion == "distribute") {
+		zielwinkel[aktivesTeil] = 0;
+		if (anzahlTeile > 2) {
+			winkelschritt = 360 / anzahlTeile;
+			rzielwinkel = winkelschritt;
+			if (rzielwinkel < sperrwinkel / 2) {
+				rzielwinkel = sperrwinkel / 2;
+				winkelschritt = (360-sperrwinkel) / (anzahlTeile - 2);
+			}
+			for (var i = 1; i < anzahlTeile; i++) {
+				zielwinkel[(aktivesTeil + i) % anzahlTeile] = rzielwinkel;
+				rzielwinkel = (rzielwinkel + winkelschritt) % 360;
+			}
+		} else {
+			if (anzahlTeile == 2) {
+				zielwinkel[(aktivesTeil+1)%2] = 180;
+			}
+		}
+		if (bewegungen.length == 0 || (bewegungen.length > 0 && bewegungen[0] != "move")) bewegungen.push("move");
+	}
+	if (aktion == "move") {
+		weiterbewegen = false;
+		for (var i = 0; i < anzahlTeile; i++) {
+			weiterbewegen = bildBewegung(i) || weiterbewegen;
+			}
+		bewegungen.push("move");
+	}
+	while ((bewegungen.length > 2) && (bewegungen[0] == bewegungen[1])) bewegungen.shift();
+	// NOTE: Debugcode entfernen
+	dp = document.getElementById("debug");
+	dp.firstChild.nodeValue= "Debug: " + bewegungen.join(", ");
+}
 
-	 this.pl = function() {
-		 hwui.pl();
-	 }
+function init() {
+	hwuielem = document.getElementById("hwui");
+	hwuielem.style.backgroundImage = "url(kreishintergrund.png)";
+	hwuielem.style.height = "300px";
+	hwuielem.style.width = "300px";
 
-	 this.mn = function() {
-		 hwui.mn();
-	 }
+	hwuielem.appendChild(auswahlrahmen());
+	hwuielem.appendChild(teilbild("fragezeichen.png", "QQsystem"));
+	hwuielem.appendChild(teilbild("teil1.png", "festplatte"));
+	hwuielem.appendChild(teilbild("teil2.png", "cdrom"));
+	hwuielem.appendChild(teilbild("teil3.png", "mouse"));
+	hwuielem.appendChild(teilbild("teil4.png", "monitor"));
+	hwuielem.appendChild(teilbild("teil5.png", "festplatte2"));
+	hwuielem.appendChild(teilbild("teil6.png", "ram"));
 
-	 this.jt = function(teil) {
-		 hwui.springeZuNamen(teil);
-	 }
+	bewegungen.push("distribute");
+	aktiv = window.setInterval("bewegen()", 100);
+}
 
-	 this.del = function() {
-		 hwui.del();
-	 }
-	 
-	 this.add = function() {
-		 hwui.add();
-	 }
- }
+function anistop() {
+	window.clearInterval(aktiv);
+}
 
-var center = 150;
-var radius = 125.0;
-var sperrwinkel = 120;
-
- var hwsysc = new hardwaresystemC();
- hwsysc.init();
+init();
