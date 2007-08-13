967aad0d073221a372f0d262b23f5262f4c43dc8
e-man <e-man@0ad31202-636c-4a70-85b4-5c1ed2c399fd>
2007-08-13 17:09:18 +0000

    Mindmaps zum Projekt und zur Datenbank erstellt

---
+++ ressourcen/Mindmaps/Banbury.mm
@@ -0,0 +1,177 @@
+<map version="0.8.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node BACKGROUND_COLOR="#ccffff" COLOR="#000000" CREATED="1187021721589" ID="Freemind_Link_1884701042" MODIFIED="1187024697671" TEXT="Banbury">
+<font NAME="SansSerif" SIZE="20"/>
+<hook NAME="accessories/plugins/AutomaticLayout.properties"/>
+<node COLOR="#0033ff" CREATED="1187021777467" ID="_" MODIFIED="1187024684328" POSITION="right" TEXT="Open">
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1187021781768" ID="Freemind_Link_1490196056" MODIFIED="1187024684328" TEXT="Equipment">
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1187021786936" ID="Freemind_Link_519453406" MODIFIED="1187024684328" TEXT="Software">
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1187021789539" ID="Freemind_Link_889013067" MODIFIED="1187024684328" TEXT="Bilder">
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1187021792001" ID="Freemind_Link_1180966397" MODIFIED="1187024684343" TEXT="Review">
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1187021794120" ID="Freemind_Link_509355942" MODIFIED="1187024684343" TEXT="Stellenmarkt">
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1187021797016" ID="Freemind_Link_1754132364" MODIFIED="1187024684343" TEXT="Kleinanzeigen">
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1187021800129" ID="Freemind_Link_540121117" MODIFIED="1187024684343" TEXT="Hardware">
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1187021804064" ID="Freemind_Link_10640876" MODIFIED="1187024684343" POSITION="left" TEXT="Login">
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1187021806169" ID="Freemind_Link_1974064843" MODIFIED="1187024684343" TEXT="Besitz">
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1187021810320" ID="Freemind_Link_1277282751" MODIFIED="1187024684343" TEXT="Hardware">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1187021812606" ID="Freemind_Link_88755641" MODIFIED="1187024684343" TEXT="Equipment">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1187021815689" ID="Freemind_Link_880218164" MODIFIED="1187024684359" TEXT="Software">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1187021817456" ID="Freemind_Link_380097612" MODIFIED="1187024684359" TEXT="Bilder">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1187021818877" ID="Freemind_Link_239027089" MODIFIED="1187024684359" TEXT="Review">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1187021822489" ID="Freemind_Link_1969052004" MODIFIED="1187024684359" TEXT="Stellenmarkt">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1187021824733" ID="Freemind_Link_277875033" MODIFIED="1187024684359" TEXT="Kleinanzeigen">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1187021833337" ID="Freemind_Link_1479725237" MODIFIED="1187024684359" TEXT="Empfehlungen">
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1187021869737" ID="Freemind_Link_481909968" MODIFIED="1187024684359" TEXT="Hardware">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1187021873834" ID="Freemind_Link_343548159" MODIFIED="1187024684359" TEXT="Equipment">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1187021876651" ID="Freemind_Link_6027852" MODIFIED="1187024684359" TEXT="Software">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1187021836546" ID="Freemind_Link_882026937" MODIFIED="1187024684359" TEXT="Profil">
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1187021840545" ID="Freemind_Link_839331179" MODIFIED="1187024684359" TEXT="Name, Vorname">
+<font NAME="SansSerif" SIZE="14"/>
+<icon BUILTIN="ksmiletris"/>
+</node>
+<node COLOR="#990000" CREATED="1187021844288" ID="Freemind_Link_731264911" MODIFIED="1187024684375" TEXT="Mail">
+<font NAME="SansSerif" SIZE="14"/>
+<icon BUILTIN="korn"/>
+</node>
+<node COLOR="#990000" CREATED="1187021847226" ID="Freemind_Link_1614857201" MODIFIED="1187024684375" TEXT="Geburtstag">
+<font NAME="SansSerif" SIZE="14"/>
+<icon BUILTIN="bell"/>
+</node>
+<node COLOR="#990000" CREATED="1187021849649" ID="Freemind_Link_957709938" MODIFIED="1187024684375" TEXT="Adresse">
+<font NAME="SansSerif" SIZE="14"/>
+<icon BUILTIN="gohome"/>
+</node>
+<node COLOR="#990000" CREATED="1187021851546" ID="Freemind_Link_484584867" MODIFIED="1187024684375" TEXT="Kurzprofil">
+<font NAME="SansSerif" SIZE="14"/>
+<icon BUILTIN="desktop_new"/>
+</node>
+<node COLOR="#990000" CREATED="1187021856233" ID="Freemind_Link_1372484532" MODIFIED="1187024684375" TEXT="Signatur">
+<font NAME="SansSerif" SIZE="14"/>
+<icon BUILTIN="pencil"/>
+</node>
+<node COLOR="#990000" CREATED="1187021858609" ID="Freemind_Link_1484860277" MODIFIED="1187024684375" TEXT="Profilbild">
+<font NAME="SansSerif" SIZE="14"/>
+<icon BUILTIN="penguin"/>
+</node>
+<node COLOR="#990000" CREATED="1187021862258" ID="Freemind_Link_1210238705" MODIFIED="1187024684375" TEXT="Kontaktformular">
+<font NAME="SansSerif" SIZE="14"/>
+<icon BUILTIN="Mail"/>
+</node>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1187021914781" ID="Freemind_Link_1193904408" MODIFIED="1187024684375" POSITION="right" TEXT="Backend">
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1187021919867" ID="Freemind_Link_1870436658" MODIFIED="1187024684390" TEXT="Gr&#xfc;n">
+<cloud COLOR="#99ff99"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1187021922235" ID="Freemind_Link_954549259" MODIFIED="1187024684390" TEXT="Neu">
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1187021925395" ID="Freemind_Link_330530315" MODIFIED="1187024684390" TEXT="Kommentar"/>
+<node COLOR="#111111" CREATED="1187021927888" ID="Freemind_Link_1574544201" MODIFIED="1187024684390" TEXT="Review"/>
+<node COLOR="#111111" CREATED="1187021929460" ID="Freemind_Link_1528472270" MODIFIED="1187024684390" TEXT="Bild"/>
+<node COLOR="#111111" CREATED="1187021965436" ID="Freemind_Link_655409560" MODIFIED="1187024684390" TEXT="Software"/>
+<node COLOR="#111111" CREATED="1187021968533" ID="Freemind_Link_1286015488" MODIFIED="1187024684390" TEXT="Hardware"/>
+<node COLOR="#111111" CREATED="1187021970767" ID="Freemind_Link_866078487" MODIFIED="1187024684390" TEXT="Anzeige"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1187021975209" ID="Freemind_Link_31112914" MODIFIED="1187024684390" TEXT="Gelb">
+<cloud COLOR="#ffff00"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1187021978172" ID="Freemind_Link_386846272" MODIFIED="1187024684390" TEXT="Neu">
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1187021979988" ID="Freemind_Link_1987475175" MODIFIED="1187024684390" TEXT="Neuer User"/>
+</node>
+<node COLOR="#990000" CREATED="1187021990416" ID="Freemind_Link_1420526239" MODIFIED="1187024684390" TEXT="Ge&#xe4;ndert">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1187021993053" ID="Freemind_Link_1534971874" MODIFIED="1187024684390" TEXT="Gel&#xf6;scht">
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1187021995118" ID="Freemind_Link_22999014" MODIFIED="1187024684390" TEXT="User gel&#xf6;scht"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1187021998909" ID="Freemind_Link_1374698649" MODIFIED="1187024684390" TEXT="Rot">
+<cloud COLOR="#ff3333"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1187022001621" ID="Freemind_Link_1195672662" MODIFIED="1187024684390" TEXT="Bild konnte nicht hochgeladen werden">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1187022008094" ID="Freemind_Link_1831401235" MODIFIED="1187024684390" TEXT="User konnte nicht erstellt werden">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1187023882813" ID="Freemind_Link_1586899261" MODIFIED="1187024684406" POSITION="left" TEXT="Ranking">
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1187023886522" ID="Freemind_Link_89131523" MODIFIED="1187024684406" TEXT="Benutzer">
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1187023899467" ID="Freemind_Link_243948306" MODIFIED="1187024684406" TEXT="Steve">
+<font NAME="SansSerif" SIZE="14"/>
+<icon BUILTIN="full-1"/>
+</node>
+<node COLOR="#990000" CREATED="1187023897474" ID="Freemind_Link_640270119" MODIFIED="1187024684406" TEXT="Woz">
+<font NAME="SansSerif" SIZE="14"/>
+<icon BUILTIN="full-2"/>
+</node>
+<node COLOR="#990000" CREATED="1187023895611" ID="Freemind_Link_1829344996" MODIFIED="1187024684406" TEXT="Phil">
+<font NAME="SansSerif" SIZE="14"/>
+<icon BUILTIN="full-3"/>
+</node>
+<node COLOR="#990000" CREATED="1187023892490" ID="Freemind_Link_528658500" MODIFIED="1187024684406" TEXT="PC">
+<font NAME="SansSerif" SIZE="14"/>
+<icon BUILTIN="full-4"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1187023905979" ID="Freemind_Link_805861265" MODIFIED="1187024684406" TEXT="Hardware">
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1187023911354" ID="Freemind_Link_1297225118" MODIFIED="1187024684406" TEXT="Aktivit&#xe4;tsindex">
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1187024620000" ID="Freemind_Link_883419529" MODIFIED="1187024684406" POSITION="right" TEXT="Datenbank">
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+</node>
+</map>
---
+++ ressourcen/Mindmaps/Datenbank.mm
@@ -0,0 +1,30 @@
+<map version="0.8.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node COLOR="#000000" CREATED="1187024713968" ID="Freemind_Link_1361374592" MODIFIED="1187024879515" TEXT="Datenbank">
+<font NAME="SansSerif" SIZE="20"/>
+<hook NAME="accessories/plugins/AutomaticLayout.properties"/>
+<node COLOR="#0033ff" CREATED="1187024719468" ID="_" MODIFIED="1187024879500" POSITION="right" TEXT="Tabelle Benutzer">
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+<node COLOR="#0033ff" CREATED="1187024725906" ID="Freemind_Link_1097087561" MODIFIED="1187024879500" POSITION="left" TEXT="Tabelle Hardware">
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+<node COLOR="#0033ff" CREATED="1187024844125" ID="Freemind_Link_705710468" MODIFIED="1187024920156" POSITION="right" TEXT="Dokumentation">
+<font NAME="SansSerif" SIZE="18"/>
+<icon BUILTIN="messagebox_warning"/>
+<node COLOR="#00b439" CREATED="1187024847656" ID="Freemind_Link_103599475" MODIFIED="1187024879515" TEXT="Tabellenname">
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1187024850890" ID="Freemind_Link_898542374" MODIFIED="1187024879515" TEXT="Feldname">
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1187024853078" ID="Freemind_Link_1191830194" MODIFIED="1187024879515" TEXT="Feldtyp"/>
+<node COLOR="#111111" CREATED="1187024856343" ID="Freemind_Link_1451074583" MODIFIED="1187024879515" TEXT="Feldbeschreibung">
+<node COLOR="#111111" CREATED="1187024862140" ID="Freemind_Link_1076357782" MODIFIED="1187024879515" TEXT="Inhalt"/>
+<node COLOR="#111111" CREATED="1187024863578" ID="Freemind_Link_802565576" MODIFIED="1187024879515" TEXT="Zweck"/>
+<node COLOR="#111111" CREATED="1187024865625" ID="Freemind_Link_1463232988" MODIFIED="1187024879515" TEXT="Verarbeitung"/>
+<node COLOR="#111111" CREATED="1187024872906" ID="Freemind_Link_625482299" MODIFIED="1187024879515" TEXT="..."/>
+</node>
+</node>
+</node>
+</node>
+</node>
+</map>
